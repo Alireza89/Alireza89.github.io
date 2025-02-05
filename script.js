b5205a87a8858edd9b90f623941e05265cb86928
Alireza89 <alireza_sameni@live.com>
2025-02-04 23:14:22 -0500

    refactor duplicated code

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 const noData = {
+    cssClass: ".n",
     backgroundColor: "#FED8B1", 
     innerHTML: "<br>&#129300 &#10067"
 };
 
 const success = {
+    cssClass: ".s",
     backgroundColor: "#B2FFBF",
     innerHTML: "<br>&#128512 &#9989",
 }
 
 const failure = {
+    cssClass: ".f",
     backgroundColor: "#E55B3C",
     innerHTML: "<br>&#128542 &#10069",
 }
 
-const nodeListN = document.querySelectorAll(".n");
-for (let i = 0; i < nodeListN.length; i++) {
-    nodeListN[i].style.backgroundColor = noData.backgroundColor;
-    nodeListN[i].innerHTML += noData.innerHTML;
+function update(e) {
+    const nodeList = document.querySelectorAll(e.cssClass);
+    for (let i = 0; i < nodeList.length; i++) {
+        nodeList[i].style.backgroundColor = e.backgroundColor;
+        nodeList[i].innerHTML += e.innerHTML;
+    }
 }
 
-const nodeListS = document.querySelectorAll(".s");
-for (let i = 0; i < nodeListS.length; i++) {
-    nodeListS[i].style.backgroundColor = success.backgroundColor;
-    nodeListS[i].innerHTML += success.innerHTML;
-}
+console.log("update begins!")
 
-const nodeListF = document.querySelectorAll(".f");
-for (let i = 0; i < nodeListF.length; i++) {
-    nodeListF[i].style.backgroundColor = failure.backgroundColor;
-    nodeListF[i].innerHTML += failure.innerHTML;
-}
+update(noData);
+update(success);
+update(failure);
+
+console.log("update Finishes!")
