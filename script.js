bd53887e96f361613552a2ca118ac309166a5390
Alireza89 <alireza_sameni@live.com>
2025-02-04 22:53:08 -0500

    write innerHTML based on class

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-let x = 5;
-console.log(`x is ${x}`) 
+const noData = {
+    backgroundColor: "#FED8B1", 
+    innerHTML: "<br>&#129300 &#10067"
+};
+
+const success = {
+    backgroundColor: "#B2FFBF",
+    innerHTML: "<br>&#128512 &#9989",
+}
+
+const failure = {
+    backgroundColor: "#E55B3C",
+    innerHTML: "<br>&#128542 &#10069",
+}
+
+const nodeListN = document.querySelectorAll(".n");
+for (let i = 0; i < nodeListN.length; i++) {
+    nodeListN[i].style.backgroundColor = noData.backgroundColor;
+    nodeListN[i].innerHTML += noData.innerHTML;
+}
+
+const nodeListS = document.querySelectorAll(".s");
+for (let i = 0; i < nodeListS.length; i++) {
+    nodeListS[i].style.backgroundColor = success.backgroundColor;
+    nodeListS[i].innerHTML += success.innerHTML;
+}
+
+const nodeListF = document.querySelectorAll(".f");
+for (let i = 0; i < nodeListF.length; i++) {
+    nodeListF[i].style.backgroundColor = failure.backgroundColor;
+    nodeListF[i].innerHTML += failure.innerHTML;
+}
